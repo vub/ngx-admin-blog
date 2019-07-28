7f268c495998ab9c227fcd0b67ce011e7bdf9366
vub <vumt92@gmail.com>
2019-07-28 19:21:32 +0700

    Create pages

---
+++ src/app/pages/writing/writing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WritingComponent } from './writing.component';
+
+
+
+@NgModule({
+  declarations: [WritingComponent],
+  imports: [
+    CommonModule,
+  ],
+})
+export class WritingModule { }
